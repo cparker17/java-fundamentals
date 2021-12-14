dde9e2ccf00715e3ba8b3ffc9a30d66b2fff100c
Chris Parker <chrisparker@chriss-air.myfiosgateway.com>
2021-12-13 19:31:10 -0500

    completed Exercise_03

--- src/labs_examples/fundamentals/labs/Exercise_03.java
+++ src/labs_examples/fundamentals/labs/Exercise_03.java
@@ -16,8 +16,9 @@ public class Exercise_03 {
 
         System.out.println("Hello World!");
 
-        // write code to print out "Check out my first program!" below
 
+        // write code to print out "Check out my first program!" below
+        System.out.println("Check out my first program!");
     }
 
 }
